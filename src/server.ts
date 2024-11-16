4252cf08d4b5810bb89f331a8753fd37f7b3588e
unknown <atikurahad@gmail.com>
2024-11-16 21:39:50 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import confiq from "./confiq";
+import app from "./app";
+import mongoose from "mongoose";
+
+async function main() {
+  try {
+    await mongoose.connect(confiq.database_url as string);
+    app.listen(confiq.port, () => {
+      console.log(`Example app listening on port ${confiq.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main()
